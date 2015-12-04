b799e9226cc6bab3add9e147f21fd75860d3cc7f
Martin von Gagern <gagern@ma.tum.de>
2015-12-04 15:32:41 +0100

    Move test file into test directory

--- test/test.js
+++ test/test.js
@@ -1,10 +1,14 @@
 var assert = require('assert');
 var gutil = require('gulp-util');
 var fs = require('fs');
-var hbs = require('./')
+var hbs = require('../');
 
-var expectedHtml = fs.readFileSync('./test/expected.html', 'utf-8');
-var json = fs.readFileSync('./test/data.json', 'utf-8');
+function testfile(relpath) {
+    return require.resolve("./" + relpath);
+}
+
+var expectedHtml = fs.readFileSync(testfile('expected.html'), 'utf-8');
+var json = fs.readFileSync(testfile('data.json'), 'utf-8');
 
 it('should generate an html document', function (done) {
 
@@ -13,7 +17,7 @@ it('should generate an html document', function (done) {
     });
 
 
-    var stream = hbs('./test/template.hbs');
+    var stream = hbs(testfile('template.hbs'));
 
     stream.on('data', function (file) {
         //fs.writeFileSync('./test/actual.html', file.contents.toString());
